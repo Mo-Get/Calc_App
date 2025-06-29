3bf65e18fdef9df87d1ba1adede2bea62db22ef3
MoGech <eyiegech@gmail.com>
2025-06-29 22:07:50 +0300

    added division and clear Back

--- main.js
+++ main.js
@@ -1,240 +1,285 @@
 // DOM Section
-const numBtn=document.querySelectorAll(".numBtn");
-const display=document.getElementById("disText");
-const equalTo=document.querySelector(".equalTo");
+const numBtn = document.querySelectorAll(".numBtn");
+const display = document.getElementById("disText");
+const equalTo = document.querySelector(".equalTo");
 
 // Assign inital values using destructuring
-let state={ 
-    firstNum:"",
-    secondNum:"",
-    operator:"",
-    firstOp:"",
-    currentOp:"",
-    result:"",
-    sum:"",
-    resultUsed:false,
-    history:[]
+let state = {
+    firstNum: "",
+    secondNum: "",
+    operator: "",
+    firstOp: "",
+    currentOp: "",
+    result: "",
+    sum: "",
+    resultUsed: false,
+    history: [],
+    numDisplay:false
 };
- 
- // Display 0 in startup 
- let initDisplay="0";
- display.textContent=initDisplay;
 
- // When the number Buttons are used
- numBtn.forEach(button => {
+// Display 0 in startup 
+let initDisplay = "0";
+display.textContent = initDisplay;
 
-    button.addEventListener("click", ()=> {   
+// When the number Buttons are used
+numBtn.forEach(button => {
 
-        const {firstNum, secondNum, operator,sum}=state;
+    button.addEventListener("click", () => {
+
+        const { firstNum, secondNum, operator, sum } = state;
 
         // For the very first flow of calculation
-     if (operator===""){
-            state = {...state, firstNum: firstNum + button.textContent,  }
-       
-        display.textContent = state.firstNum;
-    }
+        if (operator === "") {
+            state = { ...state, firstNum: firstNum + button.textContent};
 
-   else if(sum!==""){
-        reset();
-        state= {...state, firstNum:state.firstNum+button.textContent};
-        display.textContent=`${state.firstNum}`
-    }
+            state={...state, currentOp:`${state.firstNum}`}
+        }
 
-    //FirstNum is already defined
-    else{
-            state = {...state, secondNum: secondNum+button.textContent, };
-      state = {...state, currentOp: `${state.firstOp}${state.secondNum}`
-    };
+        else if (sum !== "") { 
+            reset();
+            state = { ...state, firstNum: state.firstNum + button.textContent, };
 
-      display.textContent = state.currentOp; 
-   
-      calCulate();
-    } 
+            state={...state, currentOp:`${state.firstNum}`}
+        }  
+
+        //FirstNum is already defined
+        else {
+            state = { ...state, secondNum: secondNum + button.textContent, };
+
+            state = {
+                ...state, currentOp: `${state.firstOp}${state.secondNum}`
+            };
+
+            calCulate();
+
+        }
+        
+        state={...state, numDisplay:true};
+        displayValues();
     })
-   
+
 })
 
 // When the operation buttons are used
-const opBtn=document.querySelectorAll(".opBtn")
+const opBtn = document.querySelectorAll(".opBtn")
 
-opBtn.forEach(button=> {
-    button.addEventListener("click", ()=>{
+opBtn.forEach(button => {
+    button.addEventListener("click", () => {
 
-        const {firstNum, secondNum, sum, resultUsed}=state;
+        const { firstNum, secondNum, sum, currentOp, result, resultUsed } = state;
 
- // For the very first operation flow only, secondNum not defined
-         if(firstNum!=="" && secondNum ==="" && resultUsed===false ){
-           state= {...state, operator:button.textContent,
-           firstOp:`${firstNum}${button.textContent}`};
-           display.textContent=state.firstOp;
-        
+        // For the very first operation flow only, secondNum not defined
+        if (firstNum !== "" && secondNum === "" && resultUsed === false) {
+            state = {
+                ...state, operator: button.textContent
+            };
+            state={...state, firstOp: `${firstNum}${state.operator}`}
+            
         }
 
-    //operation button is clicked more than one time for more than two numbers calculation
-        else if (firstNum!=="" && secondNum===""  && resultUsed===true){
-            state={...state, operator:button.textContent,
-                firstOp:`${state.currentOp}${button.textContent}`
+        //operation button is clicked more than one time and operation is being used for more than two numbers
+        else if (firstNum !== "" && secondNum === "" && resultUsed === true) {
+            state = {
+                ...state, operator: button.textContent,
+                }
+            state={...state, firstOp: `${currentOp}${state.operator}`
             }
-            display.textContent=state.firstOp;
-            
+
         }
 
         // if  both firstNum and SecondNum is already used
-       else  if(firstNum!=="" && secondNum!=="" && sum===""){        
-            state={    
-            ...state, operator:button.textContent,
-            firstOp:`${state.currentOp}${button.textContent}`,
-            firstNum:state.result, 
-            secondNum:"" 
+        else if (firstNum !== "" && secondNum !== "" && sum === "") {
+            state = {
+                ...state, operator: button.textContent,
+                firstNum: result,
+                secondNum: ""
             };
-            display.textContent=state.firstOp;
-            
+            state={...state, firstOp: `${currentOp}${state.operator}`}
         }
 
-         // if sum "=" button is already used
-        else if(firstNum!=="" && sum!==""){ 
-            state= {...state, operator:button.textContent,
-                
-                firstOp:`${firstNum}${button.textContent}`,
-                sum:"" ,//prevent the loop from iterating on this scope only
-                secondNum:"",
+        // if sum "=" button is already used
+        else if (firstNum !== "" && sum !== "") {
+            state = {
+                ...state, operator: button.textContent,
+                firstOp: `${firstNum}${button.textContent}`,
+                sum: "",//prevent the loop from iterating on this scope only
+                secondNum: "",
                 resultUsed: false  // when op btn is clicked more than once
             };
-            display.textContent=state.firstOp;
-          
-        } 
-       
-    }) 
-}) 
+
+        }
+
+        
+        displayValues();
+    
+    })
+})
+
+//display the values
+function displayValues(){
+const {firstNum, sum, secondNum, operator, numDisplay}=state;
+//clear button is used
+   if (firstNum===""){ 
+    display.textContent = "0";
+   }
+// display result
+   else if(sum!==""){
+    display.textContent=`=${firstNum}` ;
+   }
+// only firstnum is used for result
+   else if (firstNum !== "" && sum!=="" && secondNum === "") {
+    display.textContent=`=${firstNum}`;
+    console.log("test")
+    }
+
+//display number Buttons
+  else if (numDisplay){
+    display.textContent=state.currentOp;
+   }
+
+//display operation Buttons
+   else if (!numDisplay){
+    display.textContent=state.firstOp;
+   }
+
+   state={...state, numDisplay:false};
+}
 
 
-const reset = ()=>{
-    state={
-        firstNum:"",
-        secondNum:"",
-        operator:"",
-        result:"",
-        sum:"",
-        firstOp:"",
-        currentOp:"",
-        resultUsed:false,
+
+//reset all operation
+const reset = () => {
+    state = {
+        firstNum: "",
+        secondNum: "",
+        operator: "",
+        result: "",
+        sum: "",
+        firstOp: "",
+        currentOp: "",
+        resultUsed: false,
         history: [...state.history]
     }
-    display.textContent="0";
+    displayValues();
 }
 
 
 // When the equalTo is being used
-equalTo.addEventListener("click", ()=>{
-    const {firstNum, secondNum, operator, currentOp, result}=state;
+equalTo.addEventListener("click", () => {
+    const { firstNum, secondNum, operator, currentOp, result } = state;
 
-   if (firstNum!=="" && operator!=="" && secondNum!=="") 
-    {
-state={...state, 
-        firstNum :state.result, 
-        sum:equalTo.textContent,
-        history:[...state.history, {expr:`${currentOp}`, result}]    
-   }
-}
-   // Only firstNum is defined
-    else if(firstNum!=="" && operator==="" && secondNum==="" )
-        {
-             state={...state}   
-        } 
+    if (firstNum !== "" && operator !== "" && secondNum !== "") {
+        state = {
+            ...state,
+            firstNum: result,
+            sum: equalTo.textContent,
+            history: [...state.history, { expr: `${currentOp}`, result }]
+        }
+    }
+    // Only firstNum is defined
+    else if (firstNum !== "" && secondNum === "") {
+        state = { ...state, sum: equalTo.textContent, }
+    }
 
-<<<<<<< HEAD
-        display.textContent=` ${state.result}`;
-    
-=======
-// display the result
-        display.textContent=`=${state.firstNum}`;
->>>>>>> c43ae5369255a88c2dcd62c7bdd75db9a8ca31ec
-    })
+    // display the result
+    displayValues();
+})
 
 
 
 
-function calCulate(){
-    const {firstNum, secondNum, operator}=state;
+function calCulate() {
+    const { firstNum, secondNum, operator } = state;
     // Change the string to Number
-    let num1=Number(firstNum); 
-    let num2=Number(secondNum);
+    let num1 = Number(firstNum);
+    let num2 = Number(secondNum);
 
-// do the Calc using the operation 
+    // do the Calc using the operation 
 
-  if (firstNum!=="" && operator==="+" && secondNum!==""){
-       state={...state, result:num1+num2} 
-       
+    if (firstNum !== "" && operator === "+" && secondNum !== "") {
+        state = { ...state, result: num1 + num2 }
+
     }
 
-    else if (firstNum!=="" && operator==="x" && secondNum!==""){
-        state={...state, result:num1*num2} 
+    else if (firstNum !== "" && operator === "x" && secondNum !== "") {
+        state = { ...state, result: num1 * num2 }
     }
 
-    else if(firstNum!=="" && operator==="/" && secondNum!=="")
-        {
-            state={...state, result:num1/num2} 
-        }
-    else if(firstNum!=="" && operator==="-" && secondNum!=="")
-        {
-            state={...state, result:num1-num2} 
-        }
+    else if (firstNum !== "" && operator === "/" && secondNum === "0") {
+        state = { ...state, result: "Invalid" }
+    }
 
-     state.resultUsed=true;
-     console.log(state)
-     
+    else if (firstNum !== "" && operator === "/" && secondNum !== "") {
+        state = { ...state, result: num1 / num2 }
+    }
+
+    else if (firstNum !== "" && operator === "-" && secondNum !== "") {
+        state = { ...state, result: num1 - num2 }
+    }
+
+    state.resultUsed = true;
+
 }
 
 
 // To clear the inputs and result
+const Clear = document.querySelector(".clear")
 
-const Clear=document.querySelector(".clear")
-
-Clear.addEventListener("click", ()=>{
+Clear.addEventListener("click", () => {
     reset();
 })
 
 
 // Display History
 
-const historyBtn=document.querySelector(".historyBtn");
-const historyDisplay=document.querySelector(".historyDisplay");
+const historyBtn = document.querySelector(".historyBtn");
+const historyDisplay = document.querySelector(".historyDisplay");
 
-historyBtn.addEventListener("click", ()=>{
+historyBtn.addEventListener("click", () => {
 
-    historyDisplay.innerHTML=state.history.map((item)=>  `<div>${item.expr}=${item.result}</div>`).join("");
+    historyDisplay.innerHTML = state.history.map((item) => `<div>${item.expr}=${item.result}</div>`).join("");
 
 })
 
+
 //Clear the inputs one at a time
-/* 
-const back=document.querySelector(".back")
+function clearBack() {
+    let myArray;
+    const { firstNum, secondNum, operator } = state;
 
-back.addEventListener("click", ()=>{
-
-    if (firstNum!==""){
-        firstNum="";
-        display.textContent=firstNum;
+    if (firstNum !== "" && operator === "" && secondNum === "") {
+        myArray = firstNum.slice(0, -1)
+        state = { ...state, firstNum: myArray }
     }
-    else if(operator!==""){
-        operator="";
-        display.textContent=firstOp;
+    else if (firstNum !== "" && operator !== "" && secondNum === "") {
+        myArray = operator.slice(0, -1)
+        state = { ...state, operator: myArray }
     }
-
-    else if(firstNum!=="" && secondNum!==""){
-        secondNum="";
-        display.textContent=firstOp;
-    } 
-
+    else if (firstNum !== "" && operator !== "" && secondNum !== "") {
+        myArray = secondNum.slice(0, -1);
+        state = { ...state, secondNum: myArray };
+    }
+    
+    console.log(state);
 }
-)*/
+
+const backBtn = document.querySelector(".back");
+
+backBtn.addEventListener("click", () => {
+
+    clearBack();
+    calCulate();
+    displayValues();
+})
+
+
 
 
 // To do: the number of digits
 // more than two number operations  **Done**
 // Clear or C **Done**
 // clear one number at a time
-// when 0 is used in division
+// when 0 is used in division **Done**
 // continue the operation using the result as an input **Done**
 //using decimal fraction
 // Operation follow the Math rule
